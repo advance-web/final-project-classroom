976945c0de7b1c1cda4debb9ac7311a54cb73e0b
Thinh Le <73948433+lvthinh100@users.noreply.github.com>
2023-12-04 17:39:56 +0700

    Fix routing error on production, change social login link

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -5,6 +5,7 @@ import { Flex, Form, Input, Typography } from 'antd';
 
 import SubmitButton from '../../components/ui/SubmitButton';
 import AuthContext from '../../contexts/auth/auth-context';
+import { APP_CONFIGS } from '../../libs/constants/appConfig';
 import { setJwt } from '../../libs/utils/localStorage';
 import { signIn } from '../../services/auth';
 
@@ -132,8 +133,8 @@ export default function SignIn() {
         </Form.Item>
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
           <Flex vertical>
-            <a href="http://localhost:3000/auth/login/facebook">Đăng nhập bằng Facebook</a>
-            <a href="http://localhost:3000/auth/login/google">Đăng nhập bằng Google</a>
+            <a href={`${APP_CONFIGS.API_URL}/auth/login/facebook`}>Đăng nhập bằng Facebook</a>
+            <a href={`${APP_CONFIGS.API_URL}/auth/login/google`}>Đăng nhập bằng Google</a>
           </Flex>
         </Form.Item>
       </Form>
