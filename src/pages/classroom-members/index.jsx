421d2744c9a5d4a9ec17cde561bea8f45c62f834
tkhoang1307 <tkhoangcr.fit.hcmus@gmail.com>
2023-12-11 00:05:17 +0700

    separate role when show list participants

--- src/pages/classroom-members/index.jsx
+++ src/pages/classroom-members/index.jsx
@@ -22,20 +22,41 @@ export default function ShowClassroomMembers() {
     };
 
     getClassParticipants(idClass);
-  }, []);
+  }, [idClass]);
 
   console.log('Detail class: ', classParticipants);
+  const listTeachers = classParticipants?.filter((item) => item.role === 'teacher') || [];
+  const listStudents = classParticipants?.filter((item) => item.role === 'student') || [];
+
+  console.log('List teachers: ', listTeachers);
+  console.log('List students: ', listStudents);
+
   return (
     <div>
       <SubMenu></SubMenu>
+      <h2>Giáo viên</h2>
       <List
-        dataSource={classParticipants}
-        renderItem={(member) => (
+        dataSource={listTeachers}
+        renderItem={(teacher) => (
           <List.Item>
             <List.Item.Meta
               avatar={<Avatar icon={<UserOutlined />} />}
-              title={member.name}
-              description={member.email}
+              title={teacher.name}
+              description={teacher.email}
+            />
+          </List.Item>
+        )}
+      />
+
+      <h2>Học viên</h2>
+      <List
+        dataSource={listStudents}
+        renderItem={(student) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar icon={<UserOutlined />} />}
+              title={student.name}
+              description={student.email}
             />
           </List.Item>
         )}
